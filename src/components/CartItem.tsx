df69411d3e75fb02783104305d37014fc117433d
Danny-Kyle <dikedaniel7917@gmail.com>
2023-11-16 03:45:29 +0100

    cartItem stage1 fix

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -18,12 +18,12 @@ export function CartItem({id, quantity}: CartItemProps){
             <img src={item?.imgUrl} style={{width: "125px", height: "75px", objectFit: "cover"}}/>
             <div className="me-auto">
                 <div>
-                    {item.name} {quantity > 1 && <span className="text-muted" style={{fontSize: ".65rem"}}>x{quantity}</span> }
+                    {item?.name} {quantity > 1 && <span className="text-muted" style={{fontSize: ".65rem"}}>x{quantity}</span> }
                 </div>
                 <div className="text-muted" style={{fontSize: ".75rem"}}>
                     {formatCurrency(item.price * quantity)}
                 </div>
-                <Button variant="outline-danger" size="sm" onClick={() => removeFromCart(item?.id)}>&times;</Button>
+                <Button variant="outline-danger" size="sm" onClick={() => removeFromCart(item.id)}>&times;</Button>
             </div>
         </Stack>
     )
